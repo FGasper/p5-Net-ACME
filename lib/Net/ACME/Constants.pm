936baf6d77aedcf1a8c9144b61f5f761674f70c2
Felipe Gasper <felipe@felipegasper.com>
2016-11-30 14:22:14 -0500

    0.0922 release

--- lib/Net/ACME/Constants.pm
+++ lib/Net/ACME/Constants.pm
@@ -7,7 +7,7 @@ package Net::ACME::Constants;
 use strict;
 use warnings;
 
-our $VERSION = 0.0921;
+our $VERSION = 0.0922;
 
 our $HTTP_01_CHALLENGE_DCV_DIR_IN_DOCROOT = '.well-known/acme-challenge';
 
