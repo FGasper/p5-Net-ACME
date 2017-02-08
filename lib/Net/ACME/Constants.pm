d9406580e2767c0a0b1f5f6a50c825df182d05cc
Felipe Gasper <felipe@felipegasper.com>
2017-02-08 02:58:07 -0500

    0.1_1 - try out fixed Crypt::Perl

--- lib/Net/ACME/Constants.pm
+++ lib/Net/ACME/Constants.pm
@@ -7,7 +7,7 @@ package Net::ACME::Constants;
 use strict;
 use warnings;
 
-our $VERSION = 0.1;
+our $VERSION = '0.1_1';
 
 our $HTTP_01_CHALLENGE_DCV_DIR_IN_DOCROOT = '.well-known/acme-challenge';
 
